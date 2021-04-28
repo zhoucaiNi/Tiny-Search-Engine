73d6f2ac1c796870934d22c19cfbd98229edbcc5
Zhoucai Ni <zhoucai.ni.24@dartmouth.edu>
2021-04-28 01:26:46 -0400

    read over more instruction and started on makefile and pagedir

---
+++ common/pagedir.c
@@ -0,0 +1,44 @@
+/*
+ *
+ * pagedir.c 
+ *
+ * Zhoucai Ni, CS50 Lab 4
+ *
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "webpage.h"
+
+/******  pagedir_init ******/
+// see pegedir.h for for info
+
+bool pagedir_init(const char* pageDirectory){
+  // construct the pathname for the .crawler file in that directory 
+
+
+  // open the file for writing; on error -> return false
+
+  // close the file and return true
+
+
+
+}
+
+
+void pagedir_save(const webpage_t* page, const char* pageDirectory, const int docID){
+  // construct the pathname for the page file in pageDirectory
+
+  // open that file for writing
+
+  // print the URL
+
+  // print the depth
+
+  // print the depthprint the contents of the webpage
+
+  // close the file
+
+
+}
--- crawler/crawler.c
+++ crawler/crawler.c
@@ -7,6 +7,10 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include "hashtable.h"
+#include "bag.h"
+#include "mem.h"
+#include "webpage.h"
 
 
 static void parseArgs(const int argc, char* argv[], char** seedURL, char** pageDirectory, int* maxDepth);
@@ -14,6 +18,13 @@ static void crawl(char* seedURL, char* pageDirectory, const int maxDepth);
 static void pageScan(webpage_t* page, bag_t* pagesToCrawl, hashtable_t* pagesSeen);
 
 int main(const int argc, char* argv[]){
+  // checks for the right number of inputs // 
+  if( argc != 4 ){
+    fprintf(stderr, "pleased use a valid input :  ./crawler: seedURL pageDirectory maxDepth \n");
+    exit(1);
+  } else {
+    // runs 
+  }
 
 }
 
